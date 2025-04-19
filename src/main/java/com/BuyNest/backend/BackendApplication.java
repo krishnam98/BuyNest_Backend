a9131718f482541d56815cd77bc18438e3264bf2
krishnam98 <sonikrishnam98@gmail.com>
2025-04-19 23:11:18 +0530

    Added Environment Variables

--- src/main/java/com/BuyNest/backend/BackendApplication.java
+++ src/main/java/com/BuyNest/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.BuyNest.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv= Dotenv.load();
+		System.setProperty("Db_username",dotenv.get("Db_Username"));
+		System.setProperty("Db_password",dotenv.get("Db_Password"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
